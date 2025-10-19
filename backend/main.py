aef37e671686c0a7e340c5fceb1c1ce188330683
Robbie <dibari.roberto94@gmail.com>
2025-10-18 21:37:06 -0400

    Installed Tailwindcss, started playing with the frontend, map needs be fixed

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.gzip import GZipMiddleware
 
 app = FastAPI()
 
+# add middleware
+app.add_middleware(GZipMiddleware, minimum_size=1000)
 app.mount("/frontend", StaticFiles(directory="../frontend"), name="frontend")
 
 
--- frontend/script.js
+++ frontend/script.js
@@ -1,30 +1,67 @@
-var map = L.map("map").setView([51.505, -0.09], 13);
+class App {
+  #map;
+  #mapZoomLevel = 10;
+  #mapevent;
 
-var CartoDB_VoyagerLabelsUnder = L.tileLayer(
-  "https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png",
-  {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    subdomains: "abcd",
-  },
-).addTo(map);
+  constructor() {
+    // get user postion:
+    this._getPosition();
+  }
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert("Cannot get your position");
+        },
+      );
+    }
+  }
 
-var circle = L.circle([51.508, -0.11], {
-  color: "red",
-  fillColor: "#f03",
-  fillOpacity: 0.5,
-  radious: 5000,
-}).addTo(map);
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www/google.pt/maps/@${latitude},${longitude}`);
+    const coords = [latitude, longitude];
+    this.#map = L.map("map").setView(coords, this.#mapZoomLevel);
 
-var polygon = L.polygon([
-  [51.509, -0.08],
-  [51.503, -0.06],
-  [51.51, -0.047],
-]).addTo(map);
+    //var map = L.map("map").setView([51.505, -0.09], 13);
+    L.tileLayer(
+      "https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}{x}/{y}{r}.png",
+      {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright>OpenStreetMap</a> contributors &copy; <a href"https://carto.com/attributions">CARTO</a>',
+        subdomains: "abcd",
+      },
+    ).addTo(this.#map);
+  }
 
-var popup = L.popup()
-  .setLatLng([51.513, -0.09])
-  .setContent("I am a standalone popup.")
-  .openOn(map);
+  //     var marker = L.marker([51.5, -0.09]).addTo(map);
+
+  //     var circle = L.circle([51.508, -0.11], {
+  //       color: "red",
+  //       fillColor: "#f03",
+  //       fillOpacity: 0.5,
+  //       radious: 5000,
+  //     }).addTo(map);
+
+  //     var polygon = L.polygon([
+  //       [51.509, -0.08],
+  //       [51.503, -0.06],
+  //       [51.51, -0.047],
+  //     ]).addTo(map);
+
+  //     var popup = L.popup()
+  //       .setLatLng([51.513, -0.09])
+  //       .setContent("I am a standalone popup.")
+  //       .openOn(map);
+
+  //   function onMapClick(e) {
+  //     alert("You clicked the map at " + e.latlng);
+
+  //   map.on("click", onMapClick);
+
+  // }
+}
+const app = new App();
